265ce972900029c1f8148fcaaef584ad6c1e34b5
Claudio Cidade <claudiocidade@maximinder.com.br>
2020-02-08 15:27:22 -0300

    Removing target folder and adding more functionality to test the base class interactions

--- test-runner/src/test/java/com/laboratory/test/LaboratoryTest.java
+++ test-runner/src/test/java/com/laboratory/test/LaboratoryTest.java
@@ -1,23 +1,24 @@
 package test.java.com.laboratory.test;
 
+import main.java.com.framework.AutomatedTest;
 import main.java.com.laboratory.Laboratory;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.Assert;
 
-public class LaboratoryTest {
+public class LaboratoryTest extends AutomatedTest {
 
-    private Laboratory laboratory;
+    private final Laboratory laboratory;
 
     public LaboratoryTest() {
         this.laboratory = new Laboratory();
     }
 
-    @Test(description = "Testing the test method description for success")
+    @Test(testName = "Test1", description = "Testing the test method description for success")
     public void shouldPassSummingTwoNumbers() {
         Assert.assertTrue(laboratory.sumTwoNumbers(1, 1) == 2, "Something went wrong!");
     }
 
-    @Test(description = "Testing the test method description for failure")
+    @Test(testName = "Test2", description = "Testing the test method description for failure")
     public void shouldFailSummingTwoNumbers() {
         Assert.assertFalse(laboratory.sumTwoNumbersFail(1, 1) == 2, "Something went wrong!");
     }
