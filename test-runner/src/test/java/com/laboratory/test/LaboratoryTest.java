9815136cb7d9ae0d78398254df3c306fa9eb1840
Claudio Cidade <claudiocidade@maximinder.com.br>
2020-02-08 15:31:03 -0300

    Putitng the @Test annotation back in those test methods

--- test-runner/src/test/java/com/laboratory/test/LaboratoryTest.java
+++ test-runner/src/test/java/com/laboratory/test/LaboratoryTest.java
@@ -13,10 +13,12 @@ public class LaboratoryTest extends AutomatedTest {
         this.laboratory = new Laboratory();
     }
 
+    @Test
     public void shouldPassSummingTwoNumbers() {
         Assert.assertTrue(laboratory.sumTwoNumbers(1, 1) == 2, "Something went wrong!");
     }
 
+    @Test
     public void shouldFailSummingTwoNumbers() {
         Assert.assertFalse(laboratory.sumTwoNumbersFail(1, 1) == 2, "Something went wrong!");
     }
