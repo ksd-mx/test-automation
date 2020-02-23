b9934c4ce43a5e4a983dcab884760bd978d8f441
Claudio Cidade <claudiocidade@maximinder.com.br>
2020-02-23 12:12:58 -0300

    Results are now rendered to a json file, which can be used a test automation settings file for manual configuration or recurring test-automation proccess update automatically, as long as there is a way to associate step implementations to test case steps. This is not so simple since steps are designed in a composition fashion thus having no particular identity of its own. But I will figure out a way. Next step is to get step sequence rendered to file and make sure the test name is coming out accordingly as well

--- get-test-plan.ps1
+++ get-test-plan.ps1
@@ -2,7 +2,8 @@ param ([string]$testplan)
 
 class TestStep {
     [string] $externalId
-    [string] $name
+    [string] $action
+    [string] $outcome
 }
 
 class TestCase {
@@ -14,13 +15,16 @@ class TestCase {
 class TestGroup {
     [string] $externalId
     [string] $name
+    [TestGroup[]] $testGroupList
     [TestCase[]] $testCaseList
 }
 
-$personal_access_token = ""
-$auth_header = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($personal_access_token)")) }
-
-$env:SYSTEM_COLLECTIONURI = ""
+$auth_header = @{
+    Authorization = 'Basic ' + `
+        [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(${env:TEST_AUTOMATION_ACCESS_TOKEN})"))
+}
+$now = [System.DateTime]::Now
+Write-Host "Starting at ${now}"
 
 $suite_list = New-Object System.Collections.ArrayList($null)
 $base_uri = "${env:SYSTEM_COLLECTIONURI}/_apis"
@@ -32,9 +36,13 @@ $api_preview = "-preview.1"
 $testplan_uri = "${base_test_uri}/plans/${testplan}"
 $workitem_uri = "${base_wit_uri}/workitems"
 
+$testplan_group = [TestGroup]::New()
+
 Function GetChildSuite {
     [CmdletBinding()]
-    param([string]$suiteid)
+    param([TestGroup]$group)
+
+    $suiteid = $group.externalId
 
     $suite_uri = "${testplan_uri}/suites/${suiteid}${api50_version}"
     $suiteentry_uri = "${base_test_uri}/suiteentry/${suiteid}${api50_version}${api_preview}"
@@ -45,47 +53,62 @@ Function GetChildSuite {
     Write-Host "# Invoking SUITE DATA API @ ${suite_uri}"
     $rest_result = Invoke-RestMethod -Uri "$suiteentry_uri" -Method Get -Headers $auth_header -UseBasicParsing
 
+    $group.externalId = $suite_result.id
+    $group.name = $suite_result.name
+
     Write-host "# Getting TEST SUITES for" $suite_result.id - $suite_result.name
 
     if ($rest_result.count -gt 0 ) {
         foreach ($item in $rest_result.value ) {
             if ($item.childSuiteId -eq 0) { continue; }
+            # if ($group.testGroupList -eq $null) { $group.testGroupList = [TestGroup[]]::New() }
+            $childGroup = [TestGroup]::New()
+            $childGroup.externalId = $item.childSuiteId
 
-            GetChildSuite -suiteid $item.childSuiteId
+            $group.testGroupList += $childGroup
+
+            GetChildSuite -group $childGroup
         }
     }
 
-    GetTestCase -parent $suite_result
+    GetTestCase -parentgroup $group
 }
 
 Function GetTestCase {
     [CmdletBinding()]
-    param($parentsuite)
+    param($parentgroup)
 
-    $parentsuiteid = $parentsuite.id
+    $parentsuiteid = $parentgroup.externalId
 
     $testcase_uri = "${testplan_uri}/suites/${parentsuiteid}/testcases${api50_version}"
 
     Write-Host "# Invoking TEST CASES API @ ${testcase_uri}"
     $testcase_result = Invoke-RestMethod -Uri "$testcase_uri" -Method Get -Headers $auth_header -UseBasicParsing
 
-    Write-host "# Getting TEST CASES for" $parentsuite.id - $parentsuite.name
+    Write-host "# Getting TEST CASES for" $parentgroup.externalId - $parentgroup.name
 
     if ($testcase_result.count -gt 0) {
         foreach ($item in $testcase_result.value) {
-            GetTestCaseSteps -testcase $item
+            $newtestcase = [TestCase]::New()
+
+            $newtestcase.externalId = $item.testCase.id
+            $newtestcase.name = $item.testCase.name
+
+            $parentgroup.testCaseList += $newtestcase
+
+            GetTestCaseSteps -parenttestcase $newtestcase
         }
     }
 }
 
 Function GetTestCaseSteps {
     [CmdletBinding()]
-    param($testcase)
+    param($parenttestcase)
 
     [regex] $step_rgx = '<step id="\d+" [a-zA-Z0-9="]+>(.*?)<\/step>'
     [regex] $param_rgx = '<parameterizedString\s[a-zA-Z0-9="]+>(.*?)<\/parameterizedString>'
 
-    $testcaseid = $testcase.testCase.id
+    $testcaseid = $parenttestcase.externalId
 
     $wit_uri = "${workitem_uri}/${testcaseid}${api51_version}"
 
@@ -113,12 +136,23 @@ Function GetTestCaseSteps {
             -replace '<\/parameterizedString>' `
             -replace '\s+', ' '
 
-        Write-Host "# MATCH: ${match}"
+        if ($step_action.length -eq 0 -OR $step_outcome.length -eq 0) { continue; }
+
         Write-Host "# STEP ACTION: " $step_action
         Write-Host "# STEP OUTCOME: " $step_outcome
+
+        $newtestcasestep = [TestStep]::New()
+        $newtestcasestep.action = $step_action
+        $newtestcasestep.outcome = $step_outcome
+
+        $parenttestcase.testStepList += $newtestcasestep
     }
 }
 
 $remotetestplan = Invoke-RestMethod -Uri "${testplan_uri}" -Method Get -Headers $auth_header
 
-$result = GetChildSuite -suiteid $remotetestplan.rootSuite.id
+$testplan_group.externalId = $remotetestplan.rootSuite.id
+
+GetChildSuite -group $testplan_group
+
+$testplan_group | ConvertTo-Json -depth 100 | Out-File "result_json.json"
